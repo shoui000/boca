8ae091652751a6ff7a220da0078b53569107ff05
Dimitry Deveza <dimydeveza@gmail.com>
2025-08-06 17:57:43 -0300

    changes: muitas mudanças removido o define inutil removida a passagem por referencia refeita a funcao de ler configuracoes adicao do mapa para a struct do jogo

--- prog1/tp_free_way/main.c
+++ prog1/tp_free_way/main.c
@@ -8,7 +8,6 @@
 #define MAX_CARS_PER_ROAD 10
 #define SPRITE_MAX_WIDTH 3
 #define SPRITE_MAX_HEIGHT 2
-#define UNUSABLE_ROADS 2
 #define MAX_SPRITES 5
 
 typedef struct {
@@ -31,12 +30,13 @@ typedef struct {
   int spriteIndex;
 
   char sprites[MAX_SPRITES][SPRITE_MAX_HEIGHT][SPRITE_MAX_WIDTH];
+  char map[(MAX_ROADS * 3) + 1][MAX_COLUMNS + 4];
 
   road roads[MAX_ROADS];
 } GAME;
 
-void readConfig(GAME *g, char *path);
-void readSprites(GAME *g, char *path);
+GAME readConfig(GAME g, char *path);
+GAME readSprites(GAME g, char *path);
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -45,15 +45,13 @@ int main(int argc, char *argv[]) {
   }
 
   GAME g;
-
-  // Passagem por referencia eu posso usar ne? Vinicius ensinou (Ensinou memo, n to passando fake)
-  readConfig(&g, argv[1]);
-  readSprites(&g, argv[1]);
+  g = readConfig(g, argv[1]);
+  g = readSprites(g, argv[1]);
 
   exit(0);
 }
 
-void readConfig(GAME *g, char *path) {
+GAME readConfig(GAME g, char *path) {
   char configPath[MAX_SIZE_PATH+20];
   strcpy(configPath, path);
   strcat(configPath, "/config_inicial.txt");
@@ -66,44 +64,50 @@ void readConfig(GAME *g, char *path) {
     exit(1);
   }
 
-  // lendo o arquivo config_inicial.txt que possui a seguinte formatacao:
-  // animacao
-  // largura_mapa qtd_pistas
-  // direcao velocidade num_carro X1 X2 … Xn
-  // direcao velocidade num_carro X1 X2 … Xn
-  // G X_galinha vidas
+  char buffer[512];
+  fgets(buffer, 512, f);
+  sscanf(buffer, "%d", &g.animate);
 
-  fscanf(f, "%d\n", &g->animate);
-  int x;
-  fscanf(f, "%d %d\n", &g->mapWidth, &x);
-  g->roadsCount = x - UNUSABLE_ROADS;
+  fgets(buffer, 512, f);
+  sscanf(buffer, "%d %d", &g.mapWidth, &g.roadsCount);
 
-  char d;
-  for (int i = 0; i < g->roadsCount; i++) {
-    d = fgetc(f);
+  for (int i = 0; i < g.roadsCount; i++) {
+    fgets(buffer, 512, f);
 
-    if (d == '\n') {
-      g->roads[i].carsCount = 0;
-      g->roads[i].speed = 0;
-      g->roads[i].direction = 0;
+    if (strlen(buffer) < 2 || i == g.roadsCount-1) {
+      g.roads[i].carsCount = 0; g.roads[i].direction = 0; g.roads[i].speed = 0;
       continue;
     }
 
-    g->roads[i].direction = (d == 'D') ? 1 : -1;
-    fscanf(f, " %d %d", &g->roads[i].speed, &g->roads[i].carsCount);
+    char *token = strtok(buffer, " ");
 
-    for (int j = 0; j < g->roads[i].carsCount; j++) {
-      fscanf(f, "%d", &g->roads[i].cars[j].b);
-      fgetc(f);
+    char b;
+    sscanf(token, "%c", &b);
+    g.roads[i].direction = (b == 'D') ? 1 : -1;
+
+    token = strtok(NULL, " ");
+    sscanf(token, "%d", &g.roads[i].speed);
+
+    token = strtok(NULL, " ");
+    sscanf(token, "%d", &g.roads[i].carsCount);
+
+    for (int j = 0; j < g.roads[i].carsCount; j++) {
+      token = strtok(NULL, " ");
+      sscanf(token, "%d", &g.roads[i].cars[j].b);
     }
+
   }
 
-  fscanf(f, "G %d %d", &g->chicken.b, &g->chicken.lifes);
-  
+  fgets(buffer, 512, f);
+  sscanf(buffer, "G %d %d", &g.chicken.b, &g.chicken.lifes);
+  g.chicken.road = g.roadsCount-1;
+
   fclose(f);
+
+  return g;
 }
 
-void readSprites(GAME *g, char *path) {
+GAME readSprites(GAME g, char *path) {
   char spritesPath[MAX_SIZE_PATH+20];
   strcpy(spritesPath, path);
   strcat(spritesPath, "/personagens.txt");
@@ -118,7 +122,7 @@ void readSprites(GAME *g, char *path) {
   for (int x = 0; x < MAX_SPRITES; x++) {
     for (int i = 0; i < SPRITE_MAX_HEIGHT; i++) {
       for (int j = 0; j < SPRITE_MAX_WIDTH; j++) {
-        g->sprites[x][i][j] = fgetc(f);
+        g.sprites[x][i][j] = fgetc(f);
       }
 
       fgetc(f); // \r
@@ -127,4 +131,5 @@ void readSprites(GAME *g, char *path) {
   }
 
   fclose(f);
+  return g;
 }
